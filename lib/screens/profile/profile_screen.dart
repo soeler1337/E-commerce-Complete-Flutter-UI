ed85f409237b7c526933883eaffdae8071cdbe61
soeler <k.a.s@hotmail.de>
2022-04-03 16:24:39 +0200

    changed homescreen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/components/coustom_bottom_nav_bar.dart';
 import 'package:shop_app/enums.dart';
+import 'package:shop_app/size_config.dart';
 
 import 'components/body.dart';
 
@@ -8,6 +9,7 @@ class ProfileScreen extends StatelessWidget {
   static String routeName = "/profile";
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile"),
